087595c416c23f3999dc06e5300a8db1cf8c9d01
n1k1x86 <i@n1k1x86.ru>
2025-01-23 00:37:00 +1000

    added reading from dir and transforming into html

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module core
 
 go 1.23.0
 
-require github.com/russross/blackfriday/v2 v2.1.0 // indirect
+require github.com/russross/blackfriday/v2 v2.1.0
--- pkg/reader/reader.go
+++ pkg/reader/reader.go
@@ -1,7 +1,31 @@
 package reader
 
-import "log"
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/russross/blackfriday/v2"
+)
 
 func ReadMdFiles(dir string, output string) {
 	log.Println("[INFO] - start reading files and transform to html")
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		mdContent, err := os.ReadFile(filepath.Join(dir, file.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+		htmlConvert := blackfriday.Run(mdContent)
+		fileName := strings.Split(file.Name(), ".")[0]
+		err = os.WriteFile(filepath.Join(output, fileName+".html"), htmlConvert, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
