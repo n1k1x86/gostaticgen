1647881fc143a008b20335abd635d5f2c12c3c99
n1k1x86 <i@n1k1x86.ru>
2025-02-15 17:35:48 +1000

    [gostatic-4] added header converter

--- internal/mdconverter/mdconverter.go
+++ internal/mdconverter/mdconverter.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 )
 
 type MdConverterInterface interface {
@@ -15,6 +17,32 @@ type MdConverter struct {
 	Configs []conf.PreparedConfigs
 }
 
+func (m *MdConverter) IsHeaderPattern(line string) bool {
+	re, err := regexp.Compile(`^#{1,6}\s(\W|\d|\w)+`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return re.MatchString(line)
+}
+
+func (m *MdConverter) ReplaceHeader(line string) string {
+	hashCnt := strings.Count(line, "#")
+	newLine := strings.Replace(line, strings.Repeat("#", hashCnt)+" ", "<h1 class=\"header-1\">", 1) + "<\\h1>"
+	return newLine
+}
+
+func (m *MdConverter) ConvertToHtml(data string) {
+	for _, line := range strings.Split(data, "\r\n") {
+		if m.IsHeaderPattern(line) {
+			newLine := m.ReplaceHeader(line)
+			fmt.Println("Line:", newLine, "IsHeaderPattern?:", m.IsHeaderPattern(line))
+		} else {
+			fmt.Println("Line:", line, "IsHeaderPattern?:", m.IsHeaderPattern(line))
+		}
+
+	}
+}
+
 func (m *MdConverter) ReadMds(config conf.PreparedConfigs) {
 	confPath := config.ConfigPath
 	for _, md := range config.Yaml.Content {
@@ -22,7 +50,7 @@ func (m *MdConverter) ReadMds(config conf.PreparedConfigs) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(string(data))
+		m.ConvertToHtml(string(data))
 	}
 }
 
