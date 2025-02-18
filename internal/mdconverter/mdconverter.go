1aa1200f17e5353b020005d9c4f5fcb9c8a8425e
n1k1x86 <i@n1k1x86.ru>
2025-02-18 22:44:52 +1000

    [gostatic-4] added italic converter via regexp

--- internal/mdconverter/mdconverter.go
+++ internal/mdconverter/mdconverter.go
@@ -25,21 +25,77 @@ func (m *MdConverter) IsHeaderPattern(line string) bool {
 	return re.MatchString(line)
 }
 
+func (m *MdConverter) IsItalicPattern(line string) (bool, *regexp.Regexp) {
+	re, err := regexp.Compile(`\*{1}([\w\sа-яА-Я^\*]+?)\*{1}`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return re.MatchString(line), re
+}
+
+func (m *MdConverter) IsBoldPattern(line string) (bool, *regexp.Regexp) {
+	re, err := regexp.Compile(`\*{2}(\w|\s)+\*{2}`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return re.MatchString(line), re
+}
+
+func (m *MdConverter) IsUnOrderedListPattern(line string) (bool, *regexp.Regexp) {
+	re, err := regexp.Compile(`^-{1}\s(\W|\s|\d)+`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return re.MatchString(line), re
+}
+
+func (m *MdConverter) IsOrderedListPattern(line string) (bool, *regexp.Regexp) {
+	re, err := regexp.Compile(`^[0-9]+\.\s(\W|\d|\w)+`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return re.MatchString(line), re
+}
+
+func (m *MdConverter) IsLinkPattern(line string) (bool, *regexp.Regexp) {
+	re, err := regexp.Compile(`\[(\W|\w|\D)+\]\((\W|\w|\D)+\)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return re.MatchString(line), re
+}
+
+func (m *MdConverter) IsImgPattern(line string) (bool, *regexp.Regexp) {
+	re, err := regexp.Compile(`\!\[(\W|\w|\D)+\]\((\W|\w|\D)+\)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return re.MatchString(line), re
+}
+
 func (m *MdConverter) ReplaceHeader(line string) string {
 	hashCnt := strings.Count(line, "#")
 	newLine := strings.Replace(line, strings.Repeat("#", hashCnt)+" ", "<h1 class=\"header-1\">", 1) + "<\\h1>"
 	return newLine
 }
 
+func (m *MdConverter) ReplaceItalic(line string, re *regexp.Regexp) string {
+	var newLine string = ""
+	newLine = re.ReplaceAllString(line, "<em class=\"italic-class\">$1</em>")
+	return newLine
+}
+
 func (m *MdConverter) ConvertToHtml(data string) {
 	for _, line := range strings.Split(data, "\r\n") {
-		if m.IsHeaderPattern(line) {
+		if ok := m.IsHeaderPattern(line); ok {
 			newLine := m.ReplaceHeader(line)
-			fmt.Println("Line:", newLine, "IsHeaderPattern?:", m.IsHeaderPattern(line))
+			fmt.Println("Line:", newLine, "IsHeaderPattern?:", ok)
+		} else if ok, re := m.IsItalicPattern(line); ok {
+			newLine := m.ReplaceItalic(line, re)
+			fmt.Println("Line:", newLine, "IsItalicPattern?:", ok)
 		} else {
 			fmt.Println("Line:", line, "IsHeaderPattern?:", m.IsHeaderPattern(line))
 		}
-
 	}
 }
 
