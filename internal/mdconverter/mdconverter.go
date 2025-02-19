7db280e6126efb96a8d573f53ace3ce7ce7e9fa2
n1k1x86 <i@n1k1x86.ru>
2025-02-19 17:05:57 +1000

    [gostatic-4] added unordered list converter

--- internal/mdconverter/mdconverter.go
+++ internal/mdconverter/mdconverter.go
@@ -13,16 +13,42 @@ type MdConverterInterface interface {
 	StartConverting(mdFiles []string, confPath string)
 }
 
-type MdConverter struct {
-	Configs []conf.PreparedConfigs
+type UnOrderedListInterface interface {
+	PushLine(line string)
+	ClearQueue()
 }
 
-func (m *MdConverter) IsUnOrderedListPattern(line string) (bool, *regexp.Regexp) {
-	re, err := regexp.Compile(`^-{1}\s(\W|\s|\d)+`)
-	if err != nil {
-		log.Fatal(err)
+type UnOrderedListQueue struct {
+	Lines []*string
+}
+
+func (u *UnOrderedListQueue) PushLine(line *string) {
+	u.Lines = append(u.Lines, line)
+}
+
+func (u *UnOrderedListQueue) ClearQueue() {
+	u.Lines = make([]*string, 0)
+}
+
+func (u *UnOrderedListQueue) GetQueueLength() int {
+	return len(u.Lines)
+}
+
+func (u *UnOrderedListQueue) FormList() string {
+	resList := "<ul>\n"
+	endList := "</ul>"
+	for _, line := range u.Lines {
+		text := string([]rune(*line)[2:])
+		*line = fmt.Sprintf("<li>%s</li>", text)
+		resList += *line + "\n"
 	}
-	return re.MatchString(line), re
+	u.ClearQueue()
+	return resList + endList
+}
+
+type MdConverter struct {
+	Configs []conf.PreparedConfigs
+	UnOrderedListQueue
 }
 
 func (m *MdConverter) IsOrderedListPattern(line string) (bool, *regexp.Regexp) {
@@ -87,15 +113,39 @@ func (m *MdConverter) ReplaceImg(line *string) {
 	}
 }
 
+func (m *MdConverter) ReplaceUnOrderedList(line *string, fin bool) (bool, string) {
+	re, err := regexp.Compile(`^-{1}\s[(\W|\s|\d|\w^\-+?)]`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if re.MatchString(*line) && !fin {
+		m.PushLine(line)
+		return true, ""
+	} else if m.GetQueueLength() != 0 {
+		resList := m.FormList()
+		return false, resList
+	} else {
+		return false, *line
+	}
+}
+
 func (m *MdConverter) ConvertToHtml(data string) {
-	for _, line := range strings.Split(data, "\r\n") {
+	htmlContent := ""
+	rows := strings.Split(data, "\r\n")
+	for ind, line := range rows {
+		fin := ind == len(rows)-1
 		m.ReplaceHeader(&line)
 		m.ReplaceBold(&line)
 		m.ReplaceItalic(&line)
 		m.ReplaceImg(&line)
 		m.ReplaceLink(&line)
-		fmt.Println("Line:", line)
+		if ok, res := m.ReplaceUnOrderedList(&line, fin); ok {
+			continue
+		} else {
+			htmlContent += res + "\n"
+		}
 	}
+	fmt.Println(htmlContent)
 }
 
 func (m *MdConverter) ReadMds(config conf.PreparedConfigs) {
